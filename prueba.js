dd2eeeb83fadc8fac9d2d980f2d439d47eeecad0
Germán López Pineda <me1lopig@uco.es>
2020-11-03 22:50:11 +0100

    Actualizacion 20201103

--- prueba.js
+++ prueba.js
@@ -1,2 +1,7 @@
 // primer programa en javaScript en consola de escritorio
 console.log("Hola Mundo");
+console.log("Este es el primer programa de Javascript")
+console.log("Realizado en un IMac")
+console.log("Se ha creado un repositorio en Github")
+console.log("Se suponde que se sincroniza solo")
+
