7be8adebacf9355dec289346d0827df3668c5aa8
Germán López Pineda <me1lopig@uco.es>
2020-11-03 22:51:51 +0100

    20201103_js

--- prueba.js
+++ prueba.js
@@ -3,5 +3,5 @@ console.log("Hola Mundo");
 console.log("Este es el primer programa de Javascript")
 console.log("Realizado en un IMac")
 console.log("Se ha creado un repositorio en Github")
-console.log("Se suponde que se sincroniza solo")
+
 
